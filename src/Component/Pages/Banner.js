1ba1ebc1a54115b99215225af3df37969ff5b626
naimulislamshakil <ms0413354@gmail.com>
2022-06-09 16:36:18 +0600

    Banner section complete

--- src/Component/Pages/Banner.js
+++ src/Component/Pages/Banner.js
@@ -2,6 +2,10 @@ import React from "react";
 import "./Banner.css";
 import img from "../../img/avatar.png";
 import Navbar from "./Navbar";
+import { Link } from "react-router-dom";
+import fb from "../../img/social/facebook.png";
+import ln from "../../img/social/linkedin.png";
+import gh from "../../img/social/github.png";
 
 const Banner = () => {
   return (
@@ -11,6 +15,38 @@ const Banner = () => {
         <div class="hero-content flex-col lg:flex-row-reverse">
           <img src={img} class="max-w-sm" alt="" />
           <div>
+            <div className="mb-3">
+              <div class="avatar">
+                <div class="w-7 transition ease-in-out delay-150  hover:translate-y-2  hover:scale-100  duration-300">
+                  <a
+                    href="https://web.facebook.com/profile.php?id=100030513918924"
+                    target="_blank"
+                  >
+                    <img src={fb} alt="" />
+                  </a>
+                </div>
+              </div>
+              <div class="avatar ml-3">
+                <div class="w-7 transition ease-in-out delay-150  hover:translate-y-2  hover:scale-100  duration-300">
+                  <a
+                    href="https://www.linkedin.com/in/md-naimul-islam-134412226/"
+                    target="_blank"
+                  >
+                    <img src={ln} alt="" />
+                  </a>
+                </div>
+              </div>
+              <div class="avatar ml-3">
+                <div class="w-7 transition ease-in-out delay-150  hover:translate-y-2  hover:scale-100  duration-300">
+                  <a
+                    href="https://github.com/naimulislamshakil"
+                    target="_blank"
+                  >
+                    <img src={gh} alt="" />
+                  </a>
+                </div>
+              </div>
+            </div>
             <h5 className="text-2xl font-bold text-primary">Hello, I'm</h5>
             <h1 class="text-5xl font-bold text-secondary">Naimul Islam</h1>
             <h2 className="text-xl text-secondary">

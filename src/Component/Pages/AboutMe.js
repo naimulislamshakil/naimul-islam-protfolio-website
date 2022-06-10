8f030483ebc7934ad831b44214c4ce8ed4fccfc7
naimulislamshakil <ms0413354@gmail.com>
2022-06-10 06:43:40 +0600

    About section half compelet

--- src/Component/Pages/AboutMe.js
+++ src/Component/Pages/AboutMe.js
@@ -1,5 +1,10 @@
 import React from "react";
 import "./Banner.css";
+import AOS from "aos";
+import arrow from "../../img/arrow.png";
+import "aos/dist/aos.css"; // You can also use <link> for styles
+// ..
+AOS.init();
 
 const AboutMe = () => {
   return (
@@ -8,12 +13,33 @@ const AboutMe = () => {
         <div class="hero-content flex-col lg:flex-row">
           <img src="https://i.ibb.co/xXgcnyz/1.jpg" class="max-w-sm" alt="" />
           <div>
-            <h1 class="text-5xl font-bold">Box Office News!</h1>
-            <p class="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
+            <div data-aos="fade-left">
+              <h5 className="text-2xl font-bold text-secondary">
+                I'm A Developer
+              </h5>
+            </div>
+            <div data-aos="fade-left">
+              <h1 class="text-4xl font-bold">
+                I Can Design Anything <br /> You Want
+              </h1>
+            </div>
+            <div data-aos="fade-left">
+              <p class="py-6 dark-gray">
+                Hello there! I'm a Web Developer, and I'm very passionate and
+                dedicated to my work. I have acquired the skills and knowledge
+                necessary to make your project a success. I enjoy every step of
+                the Developer process, from discussion and collaboration.
+              </p>
+            </div>
+
+            <div>
+              <h2 data-aos="fade-left" className="text-xl font-bold">
+                Here are a Few Highlights:
+              </h2>
+              <h3>
+                <img src={arrow} alt="" />
+              </h3>
+            </div>
             <button class="btn btn-primary">Get Started</button>
           </div>
         </div>
--- src/Component/Pages/Awesome.js
+++ src/Component/Pages/Awesome.js
@@ -6,7 +6,7 @@ AOS.init();
 
 const Awesome = () => {
   return (
-    <div className="mt-16 bg-neutral">
+    <div className="mt-16">
       <div className="grid grid-cols-1 lg:grid-cols-3 mx-auto">
         <div class="w-80 mx-auto mt-5">
           <figure>
@@ -20,10 +20,10 @@ const Awesome = () => {
           </figure>
           <div class="card-body">
             <div data-aos="fade-right">
-              <h2 class="card-title mx-auto">Pixel Perfect</h2>
+              <h2 class="card-title justify-center">Pixel Perfect</h2>
             </div>
             <div data-aos="fade-right">
-              <p>
+              <p className="dark-gray">
                 Pixel perfect web development means “coding a website to match
                 the web designs pixel by pixel”. In reality, this is achievable
                 only for a fixed set of screen resolutions, so the general
@@ -45,10 +45,10 @@ const Awesome = () => {
           </figure>
           <div class="card-body">
             <div data-aos="fade-up">
-              <h2 class="card-title mx-auto">Awesome Idea</h2>
+              <h2 class="card-title justify-center">Awesome Idea</h2>
             </div>
             <div data-aos="fade-up">
-              <p>
+              <p className="dark-gray">
                 Idea development is simply about implementing different ways of
                 exploring ideas and composing solutions that best solve the
                 problem in the most effective manner.
@@ -68,10 +68,10 @@ const Awesome = () => {
           </figure>
           <div class="card-body">
             <div data-aos="fade-left">
-              <h2 class="card-title mx-auto">High Quality</h2>
+              <h2 class="card-title justify-center">High Quality</h2>
             </div>
             <div data-aos="fade-left">
-              <p>
+              <p className="dark-gray">
                 Professional development refers to continuing education and
                 career training after a person has entered the workforce in
                 order to help them develop new skills, stay up-to-date on
